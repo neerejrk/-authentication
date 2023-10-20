2bd02fcb7547d7489e796a960f8adcc0cb371fa6
neerejrk <neerej.rk@gmail.com>
2023-10-20 13:03:06 +0530

    basic setup

--- src/main/java/com/authentication/AuthenticationApplication.java
+++ src/main/java/com/authentication/AuthenticationApplication.java
@@ -1,4 +1,4 @@
-package com.neerej.authentication;
+package com.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/authentication/AuthenticationApplicationTests.java
+++ src/test/java/com/authentication/AuthenticationApplicationTests.java
@@ -1,4 +1,4 @@
-package com.neerej.authentication;
+package com.authentication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
