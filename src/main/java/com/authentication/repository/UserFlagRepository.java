2abe3c79be8cfa3e4f6237b0761a83dbae600e74
neerejrk <neerej.rk@gmail.com>
2023-10-21 11:53:24 +0530

    authentication project

--- src/main/java/com/authentication/repository/UserFlagRepository.java
+++ src/main/java/com/authentication/repository/UserFlagRepository.java
@@ -2,7 +2,6 @@ package com.authentication.repository;
 
 import com.authentication.entity.UserFlag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserFlagRepository extends JpaRepository<UserFlag, Integer> {
 
