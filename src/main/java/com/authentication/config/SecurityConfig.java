ab66df8d32a7b34bc0cb82ac67748949b9ecec7d
neerejrk <neerej.rk@gmail.com>
2023-10-21 15:57:27 +0530

    authentication project

--- src/main/java/com/authentication/config/SecurityConfig.java
+++ src/main/java/com/authentication/config/SecurityConfig.java
@@ -2,6 +2,12 @@ package com.authentication.config;
 
 import com.authentication.filter.JwtAuthFilter;
 import com.authentication.service.impl.UserInfoUserDetailsServiceImpl;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -72,4 +78,20 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("My REST API")
+                        .description("Authentication API")
+                        .version("1.0"));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 }
