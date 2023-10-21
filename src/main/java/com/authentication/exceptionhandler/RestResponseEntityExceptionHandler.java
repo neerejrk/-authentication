c63c1b652ee5e9628aaaabb815a96d1cfe5fc706
neerejrk <neerej.rk@gmail.com>
2023-10-21 10:11:20 +0530

    authentication project

---
+++ src/main/java/com/authentication/exceptionhandler/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.authentication.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    @ResponseBody
+    protected ResponseEntity<Object> handleConflict(NoSuchElementException ex) {
+        String bodyOfResponse = "One of fields id, type, value is missing";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bodyOfResponse);
+    }
+}
